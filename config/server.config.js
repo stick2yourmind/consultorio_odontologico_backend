1f8dd954379a9d6f402ee374c08d523a44eac533
Max Saravia <saravia.max.stk2@gmail.com>
2022-06-23 04:05:37 -0300

    add: vercel.json
    add: vercel.json

--- config/server.config.js
+++ config/server.config.js
@@ -5,7 +5,9 @@ const CONFIG = {
     'http://127.0.0.1:5500',
     'http://localhost:4173',
     'http://localhost:3000',
-    'https://consultorio-odontologico-j760suz9u-stick2yourmind.vercel.app/'
+    'https://consultorio-odontologico-j760suz9u-stick2yourmind.vercel.app',
+    'https://consultorio-odontologico.vercel.app',
+    process.env.ALLOWED_ORIGINS
   ],
   HOST: process.env.HOST,
   PORT: process.env.PORT
