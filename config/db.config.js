3076da6324d5327e867713627453893f971dfc7a
Max Saravia <saravia.max.stk2@gmail.com>
2022-06-24 03:32:18 -0300

    update: cfg
    update: cfg

--- config/db.config.js
+++ config/db.config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const DB_CFG = {
   MONGO: {
     OPTIONS: null,
-    URI: process.env.MONGO_URI || 'mongodb://localhost:27017/consultorio_mini'
+    URI: process.env.MONGO_URI
   }
 }
 
